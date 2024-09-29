8bab040d5b97a2595b01a19ad5bac601eef4d7f9
UseK <use.k.james@gmail.com>
2024-09-30 05:48:56 +0900

    Succeed to Aim and Drop Ball

---
+++ Assets/GameController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SpriteRenderer aimBall;
+    public GameObject dropBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MoveLeft()
+    {
+        Debug.Log("Left!!!");
+        aimBall.transform.position += new Vector3(-0.1f, 0.0f, 0.0f);
+    }
+    public void MoveRight()
+    {
+        Debug.Log("Right!!!");
+        aimBall.transform.position += new Vector3(0.1f, 0.0f, 0.0f);
+    }
+
+    public void DoDropBall()
+    {
+        var ball = Instantiate(dropBall);
+        var x = aimBall.transform.position.x;
+        var y = aimBall.transform.position.y;
+        ball.transform.position = new Vector2(x, y);
+
+
+    }
+}
